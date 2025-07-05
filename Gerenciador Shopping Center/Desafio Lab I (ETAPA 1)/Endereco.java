a5317ebe4dc3612e03892901b8f6d5cb8d6e4b5a
VICTOR GIROLDI EFFEL <victor.effel@hotmail.com>
2025-07-05 00:51:40 -0300

    atualizando metodo

--- Gerenciador Shopping Center/Desafio Lab I (ETAPA 1)/Endereco.java
+++ Gerenciador Shopping Center/Desafio Lab I (ETAPA 1)/Endereco.java
@@ -7,17 +7,7 @@ public class Endereco {
     private String numero;
     private String complemento;
 
-    public Endereco(String nomeDaRua, String cidade, String estado, String pais,
-                    String cep, String numero, String complemento) {
-        this.nomeDaRua = nomeDaRua;
-        this.cidade = cidade;
-        this.estado = estado;
-        this.pais = pais;
-        this.cep = cep;
-        this.numero = numero;
-        this.complemento = complemento;
-    }
-
+TEST
     public String toString() {
         return nomeDaRua + ", " + numero + " - " + complemento + ", " +
                cidade + " - " + estado + ", " + pais + ". CEP: " + cep;
